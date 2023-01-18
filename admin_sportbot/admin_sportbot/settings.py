17554a5b260a07d4fea95633ca26f7f43450c6b1
nazaroviktor2 <nazarov20012004@gmail.com>
2023-01-18 14:32:40 +0300

    add dnevnik

--- admin_sportbot/admin_sportbot/settings.py
+++ admin_sportbot/admin_sportbot/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'tgbot.User'
+#AUTH_USER_MODEL = 'tgbot.User'
