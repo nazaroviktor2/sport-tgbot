9c20061185f21524edb338173098d20485bf4580
nazaroviktor2 <nazarov20012004@gmail.com>
2023-01-21 16:33:31 +0300

    fix many place

--- admin_sportbot/tgbot/views.py
+++ admin_sportbot/tgbot/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from tgbot.models import User, Dnevnik, Place
+from tgbot.models import User, Dnevnik, Place, Training
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
-USERS_ON_PAGE = 20
+USERS_ON_PAGE = 5
 
 muscle_groups = {
     1: "Спина",
@@ -37,8 +37,23 @@ def add_training(request):
 
     else:
         print(request.POST)
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        user_lvl = request.POST.get("user_lvl")
+        user_gender = request.POST.get("user_gender")
+        user_type_training = request.POST.get("user_type_training")
+        places = request.POST.getlist("places")
+        user_muscles = request.POST.get("user_muscles")
+
+        training = Training(name=name, description=description, lvl=int(user_lvl),
+                            type=user_type_training, gender=user_gender, muscle_group=muscle_groups[int(user_muscles)])
+        training.save()
+        for place in places:
+            training.places.add(place)
+            training.save()
     return render(request, "add_training.html", {"places": place, "muscle_groups": muscle_groups,
                                                  "gender": gender})
+
 
 def users_page(request):
     cards = User.objects.all().order_by('id')
